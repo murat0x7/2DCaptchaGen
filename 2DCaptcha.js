c3bee43cf352539ec2ebe22e2f3ab6e630252273
Murat DEMIRCI <thinkingsecure@gmail.com>
2020-11-13 08:50:47 +0300

    Add files via upload

---
+++ 2DCaptcha.js
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en-EN">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Captcha Generator</title>
+</head>
+
+<body onload="field()">
+<div>Captcha Code :</div>
+<br />
+<canvas id="captchafield" width="200" height="50"></canvas>
+<br /><br />
+<button onclick="generatecode()">Change Code</button>
+
+<script type="text/javascript" language="javascript">
+
+var activity;
+
+function field(){
+	activity 	=	document.getElementById("captchafield").getContext("2d");
+	activity.fillStyle 	=	"#6699ff";
+	generatecode();
+}
+
+function generatecode(){
+	var text	=	"";
+	for(var firstStep = 0; firstStep<6; firstStep++){
+		var randValue	=	Math.random();
+		if(Math.round(randValue) == 0){
+			text 	+=	String.fromCharCode(48+9 * Math.random());
+		}else{
+			text 	+=	String.fromCharCode(65+25 * Math.random());
+		}
+	}
+	activity.clearRect(0, 0, 150, 50);
+	activity.fillRect(0, 0, 150, 50);
+	activity.font 	=	"30px calibri";
+	activity.strokeText(text, 24, 34);
+}
+
+</script>
+</body>
+</html>
